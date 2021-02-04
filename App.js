8c2dd8d620f8e6c24748c0fb63d0caaf7a7257a8
TooLazy-QL <qiwenluo98@gmail.com>
2021-02-04 17:45:58 -0500

    React Navigation I

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import Main from './components/MainComponent';
 
 export default function App() {
   return (
-    <Main />
+    <NavigationContainer>
+      <Main />
+    </NavigationContainer>
   );
 }   
