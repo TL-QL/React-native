5ab68b1a421d1d1a2c17a7612d1e196017caca1c
TooLazy-QL <qiwenluo98@gmail.com>
2021-02-18 18:07:11 -0500

    gesture II

--- components/DishDetailComponent.js
+++ components/DishDetailComponent.js
@@ -17,10 +17,12 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => ({
     postFavorite: (dishId) => dispatch(postFavorite(dishId)),
     postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment))
-})
+});
 
 function RenderDish(props){
     const dish = props.dish;
+
+    handleViewRef = ref => this.view = ref;
 
     const recognizeDrag = ({moveX, moveY, dx, dy}) => {
         if(dx < -200) return true;
@@ -31,6 +33,10 @@ function RenderDish(props){
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: (e, gestureState) => {
             return true;
+        },
+        onPanResponderGrant: () => {
+            this.view.rubberBand(1000)
+                .then(endState => console.log(endState.finished? 'finished' : 'cancelled'));
         },
         onPanResponderEnd: (e, gestureState) => {
             console.log("pan responder end", gestureState);
@@ -51,7 +57,7 @@ function RenderDish(props){
 
     if(dish != null) {
         return(
-            <Animatable.View animation="fadeInDown" duration={2000} delay={1000} {...PanResponder.panHandlers}>
+            <Animatable.View animation="fadeInDown" duration={2000} delay={1000} ref={this.handleViewRef} {...panResponder.panHandlers}>
                 <Card
                     featuredTitle={dish.name}
                     image={{uri: baseUrl+dish.image}}
